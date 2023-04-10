936a1cfdfa5be6d5c542712f6bd93ef6aa5d4891
ValeriiSanduliak <112845911+ValeriiSanduliak@users.noreply.github.com>
2023-04-10 19:40:45 +0300

    Task1_redone

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+    static void printMenu() {
+        System.out.println("|--------------------------|");
+        System.out.println("|           MENU           |");
+        System.out.println("|--------------------------|");
+        System.out.println("| Options:                 |");
+        System.out.println("|        1. Task1          |");
+        System.out.println("|        2. Task2          |");
+        System.out.println("|        3. Exit           |");
+        System.out.println("|--------------------------|");
+    }
 
     public static void main(String[] args) {
-
-        
+        System.out.println(" Lab 4");
+        Scanner in = new Scanner(System.in);
+        int ch = 1;
+        while (ch != 4) {
+            printMenu();
+            System.out.print("Select option:");
+            ch = in.nextInt();
+            switch (ch) {
+                case 1:
+                    Task1.task1();
+                    break;
+                //case 2 : Task2(); break;
+                case 3:
+                    return;
+            }
+        }
     }
 }
--- src/Task1.java
+++ src/Task1.java
@@ -1,8 +1,17 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
+import java.util.Set;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.TreeMap;
 import java.util.Scanner;
+
 public class Task1 {
-    static Set<String> readWordsFromFile(String words_filename){
+    static Set<String> readWordsFromFile(String words_filename) {
         Set<String> words = new HashSet<>();
         try (BufferedReader br = new BufferedReader(new FileReader(words_filename))) {
             String word;
@@ -10,12 +19,13 @@ public class Task1 {
                 words.add(word);
             }
         } catch (IOException e) {
-            System.out.println("Error message: "+ e.getMessage());
+            System.out.println("Error message: " + e.getMessage());
         }
         return words;
     }
+
     static Map<String, Integer> countWordsInFile(String text_filename, Set<String> words) {
-        Map<String, Integer> counts = new TreeMap<>();
+        Map<String, Integer> counts = new TreeMap<>((o1, o2) -> o1.toLowerCase().compareTo(o2.toLowerCase()));
         try (BufferedReader br = new BufferedReader(new FileReader(text_filename))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -28,40 +38,42 @@ public class Task1 {
                 }
             }
         } catch (IOException e) {
-            System.out.println("Error message: "+ e.getMessage());
+            System.out.println("Error message: " + e.getMessage());
         }
         return counts;
     }
 
-    public static void main(String[] args) {
+    public static void task1() {
+        System.out.println("----------------------------");
+        System.out.println("\t\tTask1 : ");
+        System.out.println("----------------------------");
         Scanner in = new Scanner(System.in);
         System.out.println("Word path: D:\\Education\\2-course\\Java\\jlab4-ValeriiSanduliak\\files\\word.txt");
-        System.out.print("Input wordsFile path:");
+        System.out.print("Input words file path:");
         String words_filename = in.next();
         File words_file = new File(words_filename);
         while (!words_file.exists()) {
-            System.out.print("File not found. Input wordsFile path:");
+            System.out.print("File not found. Input words file path:");
             words_filename = in.next();
             words_file = new File(words_filename);
         }
         System.out.println("-----------------------------------");
         System.out.println("Text path: D:\\Education\\2-course\\Java\\jlab4-ValeriiSanduliak\\files\\text.txt");
-        System.out.print("Input wordFile path:");
+        System.out.print("Input text file path:");
         String text_filename = in.next();
         File text_file = new File(text_filename);
         while (!text_file.exists()) {
-            System.out.print("File not found. Input textFile path:");
+            System.out.print("File not found. Input text file path:");
             text_filename = in.next();
             text_file = new File(text_filename);
         }
         Set<String> words = readWordsFromFile(words_filename);
         Map<String, Integer> countsWord = countWordsInFile(text_filename, words);
-        Map<String,Integer> reversCountWord = new TreeMap<>(Comparator.reverseOrder());
+        Map<String, Integer> reversCountWord = new TreeMap<>((o1, o2) -> o2.toLowerCase().compareTo(o1.toLowerCase()));
         reversCountWord.putAll(countsWord);
-        for(Map.Entry<String,Integer> i : reversCountWord.entrySet()){
-            System.out.println(i.getKey()+" : "+i.getValue());
+        for (Map.Entry<String, Integer> i : reversCountWord.entrySet()) {
+            System.out.println(i.getKey() + " : " + i.getValue());
         }
-
         System.out.println("-----------------------------------");
         System.out.println("Text path: D:\\Education\\2-course\\Java\\jlab4-ValeriiSanduliak\\files\\output.txt");
         System.out.print("Input output file path:");
@@ -72,14 +84,14 @@ public class Task1 {
             output_filename = in.next();
             output_file = new File(output_filename);
         }
-
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(output_file))) {
             for (Map.Entry<String, Integer> i : countsWord.entrySet()) {
                 bw.write(i.getKey() + " : " + i.getValue());
                 bw.newLine();
             }
         } catch (IOException e) {
-            System.out.println("Error message: "+ e.getMessage());
+            System.out.println("Error message: " + e.getMessage());
         }
+        System.out.println("End!!!");
     }
 }
